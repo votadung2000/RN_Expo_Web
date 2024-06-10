5db69c9778423ba0d479eea8db03490ee916dfdd
votadung <dung.vo@vtl-vtl.com>
2024-06-10 16:19:00 +0700

    add animation for emoji sticker

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { StyleSheet } from 'react-native'
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Home from './src/modules/Home'
 
 const App = () => {
   return (
-    <Home />
+    <GestureHandlerRootView style={styles.container}>
+      <Home />
+    </GestureHandlerRootView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
 
 export default App
--- src/components/EmojiSticker.js
+++ src/components/EmojiSticker.js
@@ -1,15 +1,65 @@
-import { View, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 const EmojiSticker = ({ imageSize, stickerSource }) => {
+    const scaleImage = useSharedValue(imageSize);
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+
+    const doubleTap = Gesture.Tap()
+        .numberOfTaps(2)
+        .onStart(() => {
+            if (scaleImage.value !== imageSize * 2) {
+                scaleImage.value = scaleImage.value * 2;
+            }
+        });
+
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value),
+        };
+    });
+
+    const drag = Gesture.Pan()
+        .onChange((event) => {
+            translateX.value += event.changeX;
+            translateY.value += event.changeY;
+        });
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: translateX.value,
+                },
+                {
+                    translateY: translateY.value,
+                },
+            ],
+        };
+    });
+
     return (
-        <View style={{ top: -350 }}>
-            <Image
-                source={stickerSource}
-                resizeMode="contain"
-                style={{ width: imageSize, height: imageSize }}
-            />
-        </View>
+        <GestureDetector gesture={drag}>
+            <Animated.View style={[containerStyle, styles.container]}>
+                <GestureDetector gesture={doubleTap}>
+                    <Animated.Image
+                        source={stickerSource}
+                        resizeMode="contain"
+                        style={[imageStyle, { width: imageSize, height: imageSize }]}
+                    />
+                </GestureDetector>
+            </Animated.View>
+        </GestureDetector>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        top: -350,
+    },
+})
 
 export default EmojiSticker
